d1c8f755bc25d23de9e414e66c1174c248d27b5c
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-10-24 22:01:26 +0530

    dainamick api mack

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const items =require('./data/item.json');
+const corce = require('./data/corce.json')
 
 
 app.get('/', (req, res) => {
     res.send('News API Running');
 });
+
+app.get('/items', (req, res) => {
+    res.send(items);
+});
+
+app.get('/item/:id' , (req, res)=>{
+    const id = req.params.id;
+    const oneprodect = corce.find(p => p.id == id);
+    res.send(oneprodect)
+})
 
 
 app.listen(port, () => {
